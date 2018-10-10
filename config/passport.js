2e48ab8906d079361c6805e0b2c44b125e9e04d2
Dandara Sousa <dandara.sousa@ccc.ufcg.edu.br>
2018-10-09 22:43:23 -0300

    resolver problema no passport

---
+++ config/passport.js
@@ -0,0 +1,28 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const logger = require("heroku-logger");
+
+
+const User = mongoose.model("User");
+const keys = require("./keys.js");
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+
+          return done(null, false);
+        })
+        .catch(err => logger.error(err));
+    })
+  );
+};
